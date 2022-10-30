b3e97184866c6be1eeac9f60c1f6b32c150563a5
apple <apple@apples-MacBook.local>
2022-10-30 22:42:50 +0100

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL || "https://eth-rinkbey/example";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "Oxkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  solidity: "0.8.17",
+  localhost: {
+    url: "http://127.0.0.1:8545/",
+    chainId: 31337,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
+};
